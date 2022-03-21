4f079185e8b1b892dcaa2acb1c33361d39024b7a
rahalrazika <rahalraika@gmail.com>
2022-03-21 19:55:04 +0100

    refactor the code with function components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export class Counter extends Component {
-  constructor(props) {
+const  Counter = () =>  {
+
+  const [counter, setCounter] = useState(0)
+ /*  constructor(props) {
     super(props);
     this.state = {counter: 0};
-  }
- increments = () => {
-  this.setState({counter: this.state.counter + 1})
+  } */
+ const increments = () => {
+  setCounter(counter + 1)
   }
 
-decrements = () => {
-    this.setState({counter: this.state.counter - 1})
+const decrements = () => {
+  setCounter(counter - 1)
 }
-  render() {
-    return (
-      <div>
+
+return (
+  <div>
+   <div>
           <h1 className='title'>Simple Counter</h1>
-          <div className='counter'>{this.state.counter}</div>
-          <button className='btn btn-inc' onClick={this.increments}>Increments</button>
-          <button className='btn btn-desc' onClick={this.decrements}>Decrements</button>
+          <div className='counter'>{counter}</div>
+          <button className='btn btn-inc' onClick={increments}>Increments</button>
+          <button className='btn btn-desc' onClick={decrements}>Decrements</button>
 
 
       </div>
-    )
-  }
+
+  </div>
+)
+
 }
 
 export default Counter
